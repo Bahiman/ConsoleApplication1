67d54d6682d204619f4cab55b34308da4fd31507
bahge <bahgez14032009@gmail.com>
2022-08-03 14:41:59 +0400

    Bahram

--- ConsoleApplication1/Source1.cpp
+++ ConsoleApplication1/Source1.cpp
@@ -1,56 +1,48 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
 #include <unordered_map>
 #include<fstream>
-//#include<ofstream>
 
-using namespace std;
-
-int main()
-{
-    //string text;
-
-    //std::cout << "Hello World!\n";
-
-    //cout << "Please give ur name" << endl;
-
-    //string strname;
-
-    //// getline(cin, strname);
-
-    //cin >> strname;
-
-    //unordered_map<int, string > statusMessages{};
-
-    //string myText;
-
-    //statusMessages.insert({ 13, "I am gay" }); 
-
-    //ifstream myReadFile("snickers.txt");
+using namespace std;    
 
 
-    //ofstream Myfile("snickers.txt");
+int main() {	// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-    //Myfile << "Files are gay";
+    string text;
 
-    //myReadFile.close();
+    std::cout << "hello world!\n";
 
-      // Create a text file
+    cout << "please give ur name" << endl;
+
+    string strname;
+
+    // getline(cin, strname);
+
+    cin >> strname;
+
+    unordered_map<int, string > statusmessages{};
+
+    string mytext;
+
+    statusmessages.insert({ 13, "i am gay" }); 
+
+    ifstream myreadfile("snickers.txt");
+
+    ofstream myfile("snickers.txt");
+
+    myfile << "files are gay";
+
+    myreadfile.close();
 
 
-    // Create a text string, which is used to output the text file
+
+    // create a text string, which is used to output the text file
     string myText;
-
-    //ofstream myFile;
 
 
     // Read from the text file
     fstream MyReadFile("filename.txt");
 
-   
     ifstream f("filename.txt");
 
     fstream myFile("filename.txt");
@@ -61,7 +53,7 @@ int main()
         cout << "This file already exists";
     }
     else {
-        
+
         myFile.open("filename.txt", ios_base::app);
 
         myFile << "Files can be tricky, but it is fun enough!";
@@ -72,7 +64,7 @@ int main()
 
     }
 
-    
+
 
     while (getline(MyReadFile, myText)) {
         // Output the text from the file
@@ -81,16 +73,16 @@ int main()
         cout << myText << "\n";
     }
 
-    //MyWriteFile << "This thing was appended";
 
     myFile.clear();                       // clear any error
     myFile.seekg(0, ios_base::end);       // move to the end of the file
     myFile << "This thing was appended\n";
 
- 
+
     cout << "This was the final change";
 
-    
+    return exits_and_can_be_opened;
+
+
 }
 
- 
